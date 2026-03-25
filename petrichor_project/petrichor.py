58c6901fcf284ef49f1cf55c84f360149767a400
Philip <ph359@kent.ac.uk>
2026-03-25 12:03:03 +0000

    [C3] Added imports - re, csv, datetime

--- petrichor_project/petrichor.py
+++ petrichor_project/petrichor.py
@@ -1,3 +1,10 @@
 # ==============================================================================================================
 # Petrichor is a menu-based weather logging programme that allows users to; log, view, and search weather data 
 # ==============================================================================================================
+
+#Imports 
+import re       # Used to validate users input      
+import csv      # Used for saving and loading data to .csv files
+import datetime # Automatically gets today's date
+
+
