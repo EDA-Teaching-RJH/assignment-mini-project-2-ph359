7ce3daa460548c3cf0ca6dd88c695e06cf1b9784
Philip <ph359@kent.ac.uk>
2026-03-26 03:20:16 +0000

    [C8] Added Class 3 Atmospheric readings, added comments to previous class. Class 3 is defined

--- petrichor_project/petrichor.py
+++ petrichor_project/petrichor.py
@@ -8,7 +8,7 @@ import re       # Used to validate users input
 import csv      # Used for saving and loading data to .csv files
 import datetime # Automatically gets today's date
 
-# CLASS 1 - Weather Entry
+# CLASS 1 - Weather Entry (Superclass)
 class WeatherLog:
     VALID_CONDITIONS = ["Clear", "Cloudy", "Fog", "Drizzle", "Rain", 
                         "Heavy Rain", "Snow", "Thunderstorm"]       
@@ -23,20 +23,42 @@ class WeatherLog:
         # prints out entry 
         return f"[{self.date}] {self.city} - {self.condtion}"                
                         
-# CLASS 2 - Addtional Entry 
+# CLASS 2 - Addtional Entry (subclass)
+# Adds temperature, humidity, and Wind Speed 
 class WeatherObservation(WeatherLog):
     
-    def __init__(self, city, date, condition, temperature, humidity,
-                 wind_speed):
+    def __init__(self, city, date, condition, temperature, 
+                 humidity, wind_speed):
+        
         super().__init__(city, date, condition)
-        #Super() calls __init__ from 'Weather Entry' so we dont have to write existing data, e.g. city, date, condition
+        # Super() calls __init__ from 'Weather Log' so we dont have to write existing data, e.g. city, date, condition
         self.temperature = temperature
         self.humidity = humidity
         self.wind_speed = wind_speed
     
     def __str__(self):
-        # Overrides the print in 'Weather Entry'to include additional details
+        # Overrides the print in 'Weather Log'to include additional details
         return (f"[{self.date}] {self.city} - {self.condition} |" 
                 f"[Temp: {self.temperature}C] |"    
                 f"Humidity: {self.humidity}% |"
                 f"Wind Speed: {self.wind_speed}kn/kt |")
+    
+# CLASS 3 - Atmospheric Reading (Subclass)
+# Adds visibiltiy, pressure, and the Air quality Index(AQI)
+class AtmosphericReading(WeatherLog):
+
+    def __init__(self, city, date, condition, visibility, 
+                 pressure, aqi):
+        # Super() Calls __init__ from 'Weather Log'
+        super().__init__(city, date, condition)
+        self.visibility = visibility
+        self.pressure = pressure
+        self.aqi = aqi 
+
+    def __str__(self):
+        # Overrides the print in 'Weather Log' to include extra measurements
+        return (f"[{self.date}] {self.city} - {self.condition} |"
+                f"Visibility: {self.visibility} km |"
+                f"Pressure: {self.pressure} hPa |"
+                f"AQI: {self.aqi}")
+
